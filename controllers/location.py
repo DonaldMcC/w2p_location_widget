f58838ddd3781bba36b28ad440b3f135c0cf076f
DonaldMcC <donaldm2020@gmail.com>
2016-09-20 23:23:54 +0100

    mission seemingly accomplished for now at least

--- controllers/location.py
+++ controllers/location.py
@@ -1,45 +1,14 @@
 # - Coding UTF8 -
 #
-# Networked Decision Making
-# Development Sites (source code): http://github.com/DonaldMcC/gdms
-#
-# Demo Sites (Pythonanywhere)
-#   http://netdecisionmaking.com/nds/
-#   http://netdecisionmaking.com/gdmsdemo/
-#
-# License Code: MIT
-# License Content: Creative Commons Attribution 3.0
-#
-# Also visit: www.web2py.com
-# or Groups: http://groups.google.com/group/web2py
-# For details on the web framework used for this development
-#
-# With thanks to Guido, Massimo and many other that make this sort of thing
-# much easier than it used to be
+# Plugin location picker widget and lookups
+# based on development by Leonel Camara and updated with some additional options
 
-# This controller has 3 functions:
-# my_questions for reviewing progress on questions you have asked
-# my_answers for reviewing your answers
-# resovled for reviewing resolved questio
-
-"""This controller has 3 functiosns:
+"""This controller has 2 functiosns:
 index: -        simple listing of all locations now with buttons
 new_location -  for creating and shortly editing locations
-my_locations -  for creating, updating and deleting details of your locations perhaps duplicated
-                with new_location 
-locations       for seeing a list of locations that are setup
-viewlocation -  for reviewing details of a single location and links to the events that
-                are planned to take place there
+accept_location - route after acceptance
 """
 
-
-
-def index():
-    page = request.args(0, cast=int, default=0)
-    items_per_page = 20
-    limitby = (page * items_per_page, (page + 1) * items_per_page + 1)
-    locations = db(db.locn.id > 0).select(limitby=limitby)
-    return dict(locations=locations, page=page, items_per_page=items_per_page)
 
 
 def new_location():
@@ -50,10 +19,8 @@ def new_location():
               'continent', 'country', 'subdivision', 'coord', 'locn_shared']
 
     buttons = [TAG.button('Submit',_type="submit"),
-               TAG.INPUT(_TYPE='BUTTON', _id="geocode", _class=stdclass, _onclick="", _VALUE="Get Co-ordinates")]
-
-    # TAG.INPUT(_TYPE='BUTTON', _id="rev_geocode", _class=stdclass, _onclick="", _VALUE="Get Address"),
-    # ]
+               TAG.INPUT(_TYPE='BUTTON', _id="geocode", _class=stdclass, _onclick="", _VALUE="Get Co-ordinates"),
+               TAG.INPUT(_TYPE='BUTTON', _id="rev_geocode", _class=stdclass, _onclick="", _VALUE="Get Address")]
 
 
     locationid = request.args(0, default=None)
@@ -90,20 +57,3 @@ def new_location():
 def accept_location():
     response.flash = "Location Created"
     return locals()
-
-
-@auth.requires_login()
-def my_locations():
-    # thinking is users shouldn't have that many of these so this should be easy - will need to be a button
-    # to view events at this location and that this shold list all locations
-    # but not sure that this is any better than just a simple query on location\index - to be considered
-    query1 = db.locn.auth_userid == auth.user.id
-    myfilter = dict(location=query1)
-    grid = SQLFORM.smartgrid(db.locn, constraints=myfilter, searchable=False)
-    return locals()
-
-
-def viewlocation():
-    locationid = request.args(0, cast=int, default=0) or redirect(URL('index'))
-    locationrow = db(db.locn.id == locationid).select(cache=(cache.ram, 1200), cacheable=True).first()
-    return dict(locationrow=locationrow, locationid=locationid)
